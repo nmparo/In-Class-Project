8a7120fa1c8295ac37401ae9132e44a29c887b13
Nicole <nmparo@uwm.edu>
2018-12-03 19:01:35 -0600

    Aurelia5

--- client/src/modules/users.js
+++ client/src/modules/users.js
@@ -23,9 +23,64 @@ export class Users {
         this.showUserEditForm = true;
     }
 
-    async save(){
-        if(this.user && this.user.firstName && this.user.lastName 
+    async save() {
+        if (this.user && this.user.firstName && this.user.lastName
             && this.user.email && this.user.password)
             await this.users.saveUser(this.user);
-            }
+    }
+    async activate(){
+        await this.getUsers();
+    }
+
+    attached(){
+        feather.replace()
+    }    
+    
+    async getUsers(){
+        await this.users.getUsers();
+    }
+
+    newUser() {
+        this.user = {
+        firstName: "",
+        lastName: "",
+        active: true,
+        role: "user",
+        email: "",
+        password: ""
         }
+        this.openEditForm();
+        }
+
+        editUser(user){
+            this.user = user;
+            this.openEditForm();
+            }            
+        
+        openEditForm(){
+            this.showUserEditForm = true;
+            setTimeout(() => {$("#firstName").focus();}, 500);
+            }            
+
+    async save() {
+        if (this.user && this.user.firstName && this.user.lastName
+        && this.user.email && this.user.password) {
+        await this.users.saveUser(this.user);
+        await this.getUsers();
+        this.back();
+        }
+        }
+
+        async delete(){
+            if(this.user){
+            await this.users.delete(this.user);
+            await this.getUsers();
+            this.back();
+            }
+            }
+        
+    back(){
+        this.showUserEditForm = false;
+      }    
+    
+}
--- client/src/resources/data/user-object.js
+++ client/src/resources/data/user-object.js
@@ -10,11 +10,30 @@ export class User {
     }
 
     async saveUser(user) {
+        let serverResponse;
         if (user) {
-            let serverResponse = await this.data.post(user, this.USER_SERVICE);
-            return serverResponse;
+        if(user._id){
+            serverResponse = await this.data.put(user, this.USER_SERVICE);
+        } else {
+            serverResponse = await this.data.post(user, this.USER_SERVICE);
         }
-    }
+        return serverResponse;
+        }
+        }
 
+        async delete(user){
+            if(user && user._id){
+                await this.data.delete(this.USER_SERVICE + '/' + user._id)
+            }
+            }            
+
+    async getUsers(){
+                let response = await this.data.get(this.USER_SERVICE);
+                if(!response.error){
+                    this.usersArray = response;
+                } else {
+                    this.usersArray = [];
+                }
+        }        
 }
 
